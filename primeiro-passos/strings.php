2e11ef5ac5ca52b03bd3204775c305c87b005cfa
viviane <vivianedasilvasouza2020@gmail.com>
2022-12-29 17:21:25 -0300

    feat: trabalhar com quebras de linhas

---
+++ primeiro-passos/strings.php
@@ -0,0 +1,10 @@
+<?php
+
+$idade = 29;
+
+echo "olá mundo!" . PHP_EOL;/*  Alguns sistemas operacionais utilizam o caracter \n para representar uma quebra de linha. Outros, utilizam \r. Já o Windows utiliza ambos (\r\n). Utilizando a constante PHP_EOL nos é abstraída esta diferença e podemos deixar nosso código
+ funcionando de forma igual em todas as plataformas. */
+
+/* echo "\t"; */
+
+echo "minha idade é $idade anos";
